c050b727b3052b2735a119f602695b38bf106869
seedjyh <seedjyh@gmail.com>
2016-09-14 06:55:36 +0800

    修改：调整lua自动测试代码，使之更清晰。

--- test-lua/test_main.lua
+++ test-lua/test_main.lua
@@ -1,36 +1,33 @@
 ----------------------------------------------------------------------------------------------------
 --  IFComicDownload的lua脚本本地测试工具 ver 1.0
---  2016-9-5 13:19:37 by seedjyh@gmail.com
+--  2016-9-14 06:03:46 by seedjyh@gmail.com
 ----------------------------------------------------------------------------------------------------
--- 这个文件是测试lua脚本的主文件。
+-- 本文件是测试lua脚本的主文件。
 -- 共分4步。
 -- 1. 加载script/api/lua脚本，以及script/sites/www.XXX.com/*.lua（具体哪个站点需要指定）脚本。
 -- 2. 读取一个指定的html文件（事先用浏览器下载到本地）。
 -- 3. 调用想要测试的函数（不止一个，必须先Analyse再Get分析结果）
 ----------------------------------------------------------------------------------------------------
-dofile("M:\\GitHubRepositories\\IFComicDownloader\\test-lua\\test_tool.lua")
-
-------------------------------------------------------------------
--- 预测参数
--- 待测试的网站目录名（位于script/sites/下），例如'www.ikanman.com'
-local site_dir = 'www.dm5.com'
--- 待分析的网页文件名（已经被下载到本地硬盘）
-local web_file_path = "M:\\GitHubRepositories\\IFComicDownloader\\test-lua\\comic_page.html"
--- 待分析的网页URL
-local web_page_url = "http://g.e-hentai.org/s/5ea57e66ac/523011-1"
-------------------------------------------------------------------
 
 -- Step 1：加载所有待测试或测试时会用到的lua脚本。
-for _,v in pairs(GetRequiredLuaScriptList(site_dir)) do
-    dofile(v)
+-- Step 1.1：加载测试工具函数
+dofile("M:\\GitHubRepositories\\IFComicDownloader\\test-lua\\test_tool.lua")
+-- Step 1.2：加载待测试的脚本
+LoadLuaFileList(GetAllLuaFilesInPath("M:\\GitHubRepositories\\IFComicDownloader\\script\\api\\"))
+LoadLuaFileList(GetAllLuaFilesInPath("M:\\GitHubRepositories\\IFComicDownloader\\script\\sites\\www.mh160.com\\"))
+-- Step 1.3：加载table: test_case_list
+dofile("M:\\GitHubRepositories\\IFComicDownloader\\test-lua\\www.mh160.com\\profile.lua")
+
+-- Step 2：执行测试
+for case_name, case_data in pairs(test_case_list) do
+    print('Starting test case: ' .. case_name)
+    local analyse_result = case_data.function_to_test(case_data.url, ReadFile(case_data.page), "")
+    print('expected:')
+    print(ReadFile(case_data.expected_analyse_result))
+    print('actual:')
+    print(analyse_result)
 end
-
--- Step 2：加载待测试的网页文件。
 pagestr = ReadFile(web_file_path)
-
--- Step 3：调用欲测试的函数。
---print(GetPageIndexFromURL(web_page_url))
-print(ComicPageAnalyse(web_page_url, pagestr, ""))
 
 -- END.
 
--- test-lua/test_tool.lua
+++ test-lua/test_tool.lua
@@ -2,71 +2,6 @@
 --  IFComicDownload的lua脚本本地测试工具 ver 1.0
 --  2016-9-5 13:19:37 by seedjyh@gmail.com
 ----------------------------------------------------------------------------------------------------
--- 这个文件是测试lua脚本的工具函数集合。
--- 共分4步。
--- 1. 加载script/api/lua脚本，以及script/sites/www.XXX.com/*.lua（具体哪个站点需要指定）脚本。
--- 2. 读取一个指定的html文件（事先用浏览器下载到本地）。
--- 3. 调用想要测试的函数（不止一个，必须先Analyse再Get分析结果）。
--- 4. 打印运行结果。
-----------------------------------------------------------------------------------------------------
--- step 1
-function GetApiScriptList()
-    local list = {}
-    list[1 + #list] = 'lib.lua'
-    list[1 + #list] = 'eval.lua'
-    list[1 + #list] = 'site_read.lua'
-    return list
-end
-
-function GetSiteScriptList()
-    local list = {}
-    list[1 + #list] = 'comic.page.analyse.lua'
-    list[1 + #list] = 'volume.page.analyse.lua'
-    list[1 + #list] = 'picture.page.analyse.lua'
-    list[1 + #list] = 'url.analyse.lua'
-    return list
-end
-
-function GetSiteScriptList_extra()
-    local list = {}
-    return list
-end
-
-function AppendPrefixToList(prefix, list)
-    local result = {}
-    for _,v in pairs(list) do
-        result[1 + #result] = prefix .. v
-    end
-    return result
-end
-
-function ConnectTwoList(list_a, list_b)
-    local result = {}
-    for _,v in pairs(list_a) do
-        result[1 + #result] = v
-    end
-    for _,v in pairs(list_b) do
-        result[1 + #result] = v
-    end
-    return result
-end
-
--- 获取“分析某个网站的页面需要加载的lua文件”列表。
--- 参数：待分析的网站相应分析脚本的文件夹的名字，例如"www.ikanman.com"
--- 返回：一个table，key都是整数，从1开始编号。
--- 使用样例：
--- for _,v in pairs(GetRequiredLuaScriptList('www.ikanman.com')) do
---     print(v)
--- end
-function GetRequiredLuaScriptList(site_dir_name)
-    local script_root_path = 'M:\\GitHubRepositories\\IFComicDownloader\\script\\'
-    local result = {}
-    
-    result = ConnectTwoList(result, AppendPrefixToList("api\\", GetApiScriptList()))
-    result = ConnectTwoList(result, AppendPrefixToList("sites\\" .. site_dir_name .. "\\", ConnectTwoList(GetSiteScriptList(), GetSiteScriptList_extra())))
-    return AppendPrefixToList(script_root_path, result)
-end
-
 -- 读取一个文件（文件路径从参数file_path传入）并返回文件内容。
 -- 返回值：
 -- 如果成功，返回文件数据（string格式）
@@ -77,4 +12,49 @@ function ReadFile(file_path)
     local data = file:read("*all")
     file:close()
     return data
+end
+
+-- 加载一个lua文件路径列表里的所有成员
+function LoadLuaFileList(lua_file_list)
+    for _,v in pairs(lua_file_list) do
+        print('loading: ' .. v)
+        dofile(v)
+    end
+end
+
+-- 字符串拆分
+function lua_string_split(str, split_char)
+    local sub_str_tab = {};
+    local i = 0;
+    local j = 0;
+    while true do
+        j = string.find(str, split_char,i+1); -- 从目标串str第i+1个字符开始搜索指定串
+        if j == nil then
+            table.insert(sub_str_tab,str);
+            break;
+        end;
+        table.insert(sub_str_tab,string.sub(str,i+1,j-1));
+        i = j;
+    end
+    return sub_str_tab;
+end
+
+-- 获取一个目录下的所有lua文件的路径列表
+function GetAllLuaFilesInPath(path)
+    if string.sub(path, string.len(path)) ~= '\\' then
+        path = path .. '\\'
+    end
+    local handle = io.popen("ls " .. path)
+    local l = handle:read("*all")
+    handle:close()
+    local s = lua_string_split(l, '\n')
+    
+    local result_list = {}
+    for _,v in pairs(s) do
+        _, end_index = string.find(v, '.lua')
+        if end_index == string.len(v) then
+            table.insert(result_list, path .. v)
+        end
+    end
+    return result_list
 end
