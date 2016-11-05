1ce00652d3b466bc0e5e081b2ea68acfbe9a7a98
seedjyh <seedjyh@gmail.com>
2016-11-05 21:43:27 +0800

    修改：允许脚本处理多页的g.e-hentai.org漫画。

--- script/sites/g.e-hentai.org/volume.page.analyse.lua
+++ script/sites/g.e-hentai.org/volume.page.analyse.lua
@@ -9,15 +9,68 @@ function VolumeUrlAnalyse(url)
 	return VolumePageAnalyse(url, pagestr, "")
 end
 
-function VolumePageAnalyse(volume_page_url, pagestr, extra_info)
-    local function_name = "VolumePageAnalyse(\"" .. volume_page_url .."\")"
+function VolumePageAnalyse(url, pagestr, extra_info)
+    local function_name = "VolumePageAnalyse(\"" .. url .."\")"
     local result = "<result>"
     ----------------------------------------------------------------------------
     -- getting volume title
     result = result .. "<volumetitle>gallery</volumetitle>"
     ----------------------------------------------------------
     result = result .. "<pictureinfolist>"
-    -- getting picture url
+
+    -- getting sub_volume_url_list
+    local sub_volume_url_list = GetSubVolumeUrlList(pagestr)
+
+    -- handle each sub-volume URL in sub_volume_url_list
+    for i = 1, #sub_volume_url_list do
+        pic_url_list_for_subvolume, errmsg = SubVolumePageAnalyse(sub_volume_url_list[i])
+        if not pic_url_list_for_subvolume then
+            return nil, errmsg
+        end
+        for i = 1, #pic_url_list_for_subvolume do
+            result = result .. "<pictureinfo><pictureurl>" .. pic_url_list_for_subvolume[i] .. "</pictureurl></pictureinfo>"
+        end
+    end
+    ----------------------------------------------------------
+    result = result .. "</pictureinfolist>"
+    ----------------------------------------------------------------------------
+    result = result .. "</result>"
+
+    return result
+end
+
+-- 从首个VolumePage分析出所有SubVolumePage的URL。
+function GetSubVolumeUrlList(pagestr)
+    local start_index = JumpStr(pagestr, 1, "ptt", 1)
+    if not start_index then return nil, "No ptt" end
+    
+    local end_index = JumpStr(pagestr, start_index, "</tr>", 1)
+    if not start_index then return nil, "No </tr>" end
+
+    local result = {}
+    while true do
+        start_index = JumpStr(pagestr, start_index, "href=\"", 1)
+        if not start_index or start_index > end_index then
+            break
+        end
+        local now_url = GetStr(pagestr, start_index, "\"")
+        if not now_url then return nil, "Failed to get now_url" end
+        table.insert(result, now_url)
+    end
+    
+    -- 按照上面的算法，最后一个URL是“下一页”的URL，而“下一页”的URL必然是前面的URL系列中的一项，为了避免重复，要去掉这最后一项。
+    table.remove(result, #result)
+    return result
+end
+
+-- 仅仅分析一个SubVolume，返回该SubVolumePage的所有PicturePageURL的list（table, key starts from 1）
+function SubVolumePageAnalyse(url)
+    local pagestr, errmsg = DownloadURL(url, "", "")
+    if not pagestr then
+        return nil, "DownloadURL failed(url=" .. url .. ')'
+    end
+    
+    local pic_url_list = {}
     local start_index = 1
     while true do
         start_index = JumpStr(pagestr, start_index, "\"gdtm\"", 1)
@@ -26,15 +79,9 @@ function VolumePageAnalyse(volume_page_url, pagestr, extra_info)
         if type(start_index) ~= "number" then return nil, "getting picture url failed" end
         local picture_url = GetStr(pagestr, start_index, "\"")
         if type(picture_url) ~= "string" then return nil, "getting picture url failed" end
-        result = result .. "<pictureinfo><pictureurl>" .. picture_url .. "</pictureurl></pictureinfo>"
+        table.insert(pic_url_list, picture_url)
     end
-
-    ----------------------------------------------------------
-    result = result .. "</pictureinfolist>"
-    ----------------------------------------------------------------------------
-    result = result .. "</result>"
-
-    return result
+    return pic_url_list
 end
 
 function VolumePageGetVolumeTitle(analyse_result)
