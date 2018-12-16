a4ad8192a17eec26361f475ee0fbf2659ccc0b00
seedjyh <seedjyh@gmail.com>
2018-12-16 16:26:45 +0800

    修正：修正e站漫画页在没有日语标题时出错的情况。

--- script/sites/g.e-hentai.org/comic.page.analyse.lua
+++ script/sites/g.e-hentai.org/comic.page.analyse.lua
@@ -39,10 +39,15 @@ end
 
 function ComicPageAnalyse_GetComicTilte(pagestr)
     local start_index = JumpStr(pagestr, 1, "<h1 id=\"gj\">", 1)
-    if type(start_index) ~= "number" then return nil, "Jumping <h1 id=\"gj\">failed." end
+    if type(start_index) ~= "number" then start_index = JumpStr(pagestr, 1, "", 1) return nil, "Jumping <h1 id=\"gj\">failed." end
     local japanese_result = GetStr(pagestr, start_index, "<")
     if type(japanese_result) ~= "string" then return nil, "Getting until < failed." end
 
+    start_index = JumpStr(pagestr, 1, "<h1 id=\"gn\">", 1)
+    if type(start_index) ~= "number" then start_index = JumpStr(pagestr, 1, "", 1) return nil, "Jumping <h1 id=\"gn\">failed." end
+    local english_result = GetStr(pagestr, start_index, "<")
+    if type(english_result) ~= "string" then return nil, "Getting until < failed." end
+	
     if string.len(japanese_result) > 0 then
         return japanese_result
     else
